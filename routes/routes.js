7b6caef6be1525ba1ed1de667eeb8b18f217bd4d
collins <collinsolayemi@gmail.com>
2023-01-16 06:32:00 -0800

    code refacturing

--- routes/routes.js
+++ routes/routes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const { dashBoard, logIn } = require("../controllers/main");
 const router = express.Router();
 
-router.route("/dashbpard").get(dashBoard);
+router.route("/dashboard").get(dashBoard);
 router.route("/login").post(logIn);
 
 module.exports = router;
