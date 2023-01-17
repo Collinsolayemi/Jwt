1ee206080025c3be88892424c86ef21a0aacfa33
collins <collinsolayemi@gmail.com>
2023-01-16 16:43:49 -0800

    added more functionality

---
+++ middleware/auth.js
@@ -0,0 +1,6 @@
+const aunthenticationMiddleware = async (req, res, next) => {
+  console.log(req.headers.authorization);
+  next();
+};
+
+module.exports = aunthenticationMiddleware;
--- routes/routes.js
+++ routes/routes.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const { dashBoard, logIn } = require("../controllers/main");
 const router = express.Router();
+const aunthenticationMiddleware = require("../middleware/auth");
 
-router.route("/dashboard").get(dashBoard);
+router.route("/dashboard").get(aunthenticationMiddleware, dashBoard);
 router.route("/login").post(logIn);
 
 module.exports = router;
