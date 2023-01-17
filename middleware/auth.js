4977c0707a692e78115142ccf97d925ee6173374
collins <collinsolayemi@gmail.com>
2023-01-17 04:05:20 -0800

    added more functionality

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,6 +1,25 @@
-const aunthenticationMiddleware = async (req, res, next) => {
-  console.log(req.headers.authorization);
-  next();
-};
+{
+  const CustomApiError = require("../error/custom-error");
+  const jwt = require("jsonwebtoken");
 
-module.exports = aunthenticationMiddleware;
+  const aunthenticationMiddleware = async (req, res, next) => {
+    const authHeader = req.headers.authorization;
+    if (!authHeader || !authHeader.startsWith("Bearer")) {
+      throw new CustomApiError("Aunthentication error, no token provided", 401);
+      next();
+    }
+
+    const token = authHeader.split(" ")[1];
+
+    try {
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      const { id, username } = decoded;
+      req.user = { id, username };
+      next();
+    } catch (err) {
+      throw new CustomApiError("Not authorize to access this route", 401);
+    }
+  };
+
+  module.exports = aunthenticationMiddleware;
+}
--- routes/routes.js
+++ routes/routes.js
@@ -1,9 +1,10 @@
 const express = require("express");
+
 const { dashBoard, logIn } = require("../controllers/main");
 const router = express.Router();
-const aunthenticationMiddleware = require("../middleware/auth");
+const authMiddleware = require("../middleware/auth");
 
-router.route("/dashboard").get(aunthenticationMiddleware, dashBoard);
+router.route("/dashboard").get(authMiddleware, dashBoard);
 router.route("/login").post(logIn);
 
 module.exports = router;
